8387699a44c264f54ec1c64bd39cc9a11f17eaa3
刘建 <v-liujian@shuwen.com>
2020-06-12 16:30:25 +0800

    测试pro --test

---
+++ t.js
@@ -0,0 +1,16 @@
+const action = "install";
+switch (action) {
+  case "install":
+    console.log("install");
+    break;
+  case "test":
+    console.log("test");
+    break;
+  case "pro":
+    console.log("pro");
+    break;
+
+  default:
+    console.log("nothing");
+    break;
+}
