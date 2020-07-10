170ba8dd7327ff8609a673ce9cff4acb5559fa42
刘健 <17602656463@163.com>
2020-07-10 10:21:46 +0800

    saga入门

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,3 +1,17 @@
-export default function* helloSaga() {
+import { delay, put, takeEvery, all } from "redux-saga/effects";
+
+function* incrementAsync() {
+  yield delay(1000);
+  yield put({ type: "INCREMENT" });
+}
+
+export function* helloSaga() {
   console.log("Hello Sagas!");
+}
+
+export function* async() {
+  yield takeEvery("INCREMENT_ASYNC", incrementAsync);
+}
+export default function* root() {
+  yield all([helloSaga(), async()]);
 }
