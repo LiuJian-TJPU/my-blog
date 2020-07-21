352213af5d9ecf5145876abbd9d7047935055ef6
刘健 <17602656463@163.com>
2020-07-21 11:02:25 +0800

    proxy

--- build/utils.js
+++ build/utils.js
@@ -1,11 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const resolve = p => path.resolve(__dirname, '../', p);
+const resolve = (p) => path.resolve(__dirname, "../", p);
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 
+const api_env = process.env.API_ENV;
+
+const api_env_list = {
+  online: {
+    "/api": "http://liujiancn.cn:3000",
+  },
+  locale: {
+    "/api": "http://localhost:3000",
+  },
+};
+
+const proxy = api_env_list[api_env];
 
 module.exports = {
   isDev,
-  resolve
-}
+  resolve,
+  proxy,
+};
